8ced47c2d76e2d9f6df0a64f88abe1a823d0be47
Leonard Choo <leochoodev@gmail.com>
2021-07-22 09:50:05 +0900

    tutorial part 2

---
+++ src/pages/gallery.js
@@ -0,0 +1,12 @@
+import Frame from "../components/frame";
+import * as React from "react";
+
+const gallery = () => {
+  return (
+    <Frame>
+      <p>This will be pass in as children</p>
+    </Frame>
+  );
+};
+
+export default gallery;
