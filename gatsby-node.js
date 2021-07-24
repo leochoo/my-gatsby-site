ce03e289368248afbf5cdb5b6d684736c4b2e6dd
Leonard Choo <leochoodev@gmail.com>
2021-07-24 17:11:18 +0900

    fixing. fixed gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,66 @@
-// Nodejs native utility module!
-// the path module provides utilities for working with file and directory paths. It can be accessed using:
-const path = require(`path`);
+const { createFilePath } = require("gatsby-source-filesystem");
 
-const { createFilePath } = require(`gatsby-source-filesystem`);
-
-exports.onCreateNode = ({ node, getNode, actions }) => {
+exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
-  if (node.internal.type === `Mdx`) {
-    const slug = createFilePath({ node, getNode, basePath: `blog` });
-    console.log("slug created", slug);
+
+  // you only want to operate on `Mdx` nodes. If you had content from a
+  // remote CMS you could also check to see if the parent node was a
+  // `File` node here
+  if (node.internal.type === "Mdx") {
+    const value = createFilePath({ node, getNode });
+
     createNodeField({
+      // Name of the field you are adding
+      name: "slug",
+      // Individual MDX node
       node,
-      name: `slug`,
-      value: slug,
+      // Generated value based on filepath with "blog" prefix. you
+      // don't need a separating "/" before the value because
+      // createFilePath returns a path with the leading "/".
+      value: `/blog${value}`,
     });
   }
 };
 
-exports.createPages = async ({ graphql, actions }) => {
+const path = require("path");
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  // Destructure the createPage function from the actions object
   const { createPage } = actions;
+
   const result = await graphql(`
     query {
       allMdx {
-        nodes {
-          slug
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+          }
         }
       }
     }
   `);
-  result.data.allMdx.nodes.forEach(({ slug }) => {
+
+  if (result.errors) {
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
+  }
+
+  // Create blog post pages.
+  const posts = result.data.allMdx.edges;
+
+  // you'll call `createPage` for each result
+  posts.forEach(({ node }, index) => {
     createPage({
-      path: slug,
+      // This is the slug you created before
+      // (or `node.frontmatter.slug`)
+      path: node.fields.slug,
+      // This component will wrap our MDX content
       component: path.resolve(`./src/templates/blog-post.js`),
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: slug,
-      },
+      // You can use the values in this context in
+      // our page layout component
+      context: { id: node.id },
     });
   });
 };
